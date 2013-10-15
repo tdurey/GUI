f5d7aeddb6a893e28e8b8eac1f6c23eb4c5d2e43
Thane Durey <tdurey@harding.edu>
2013-10-15 16:10:06 -0500

    Added GUI files

---
+++ PrimeNumbers-GUI/PrimeNumbers-GUI/MainForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace PrimeNumbers_GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startNumTextBox = new System.Windows.Forms.TextBox();
+            this.endNumTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numbersTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prime numbers:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Range:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 59);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "to";
+            // 
+            // startNumTextBox
+            // 
+            this.startNumTextBox.Location = new System.Drawing.Point(21, 31);
+            this.startNumTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startNumTextBox.Name = "startNumTextBox";
+            this.startNumTextBox.Size = new System.Drawing.Size(132, 22);
+            this.startNumTextBox.TabIndex = 3;
+            this.startNumTextBox.Text = "1000";
+            // 
+            // endNumTextBox
+            // 
+            this.endNumTextBox.Location = new System.Drawing.Point(21, 79);
+            this.endNumTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.endNumTextBox.Name = "endNumTextBox";
+            this.endNumTextBox.Size = new System.Drawing.Size(132, 22);
+            this.endNumTextBox.TabIndex = 4;
+            this.endNumTextBox.Text = "4000";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(21, 158);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(133, 28);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // numbersTextBox
+            // 
+            this.numbersTextBox.Location = new System.Drawing.Point(192, 31);
+            this.numbersTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numbersTextBox.Multiline = true;
+            this.numbersTextBox.Name = "numbersTextBox";
+            this.numbersTextBox.ReadOnly = true;
+            this.numbersTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.numbersTextBox.Size = new System.Drawing.Size(169, 154);
+            this.numbersTextBox.TabIndex = 6;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(21, 207);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 28);
+            this.startButton.TabIndex = 7;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Enabled = false;
+            this.pauseButton.Location = new System.Drawing.Point(141, 207);
+            this.pauseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(100, 28);
+            this.pauseButton.TabIndex = 8;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(263, 207);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 250);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.numbersTextBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.endNumTextBox);
+            this.Controls.Add(this.startNumTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Prime Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox startNumTextBox;
+        private System.Windows.Forms.TextBox endNumTextBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox numbersTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ PrimeNumbers-GUI/PrimeNumbers-GUI/MainForm.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrimeNumbers_GUI
+{
+    public partial class MainForm : Form
+    {
+        private bool cancelJob = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Prevent user from messing with certain controls while job is running
+            
+            progressBar1.Visible = true;
+            cancelButton.Enabled = true;
+            pauseButton.Enabled = true;
+            pauseButton.Text = "Pause";
+            startNumTextBox.Enabled = false;
+            endNumTextBox.Enabled = false;
+            startButton.Enabled = false;
+
+            numbersTextBox.Clear();
+
+            int first;
+            int last;
+
+            // Find all prime numbers starting between the first and last numbers
+            bool isFirstNum = Int32.TryParse(startNumTextBox.Text, out first);
+            bool isLastNum = Int32.TryParse(endNumTextBox.Text, out last);
+
+            if (isFirstNum && isLastNum)
+            {
+                if (first > 0 && last > 0)
+                {
+                    if (first > last)
+                    {
+                        last = Convert.ToInt32(startNumTextBox.Text);
+                        first = Convert.ToInt32(endNumTextBox.Text);
+                    }
+
+                    progressBar1.Minimum = first;
+                    progressBar1.Maximum = last;
+
+                    if (!backgroundWorker1.IsBusy)
+                    {
+                        backgroundWorker1.RunWorkerAsync();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Number must be larger than 0.");
+
+                    startNumTextBox.Enabled = true;
+                    endNumTextBox.Enabled = true;
+                    startButton.Enabled = true;
+                    progressBar1.Visible = false;
+                    cancelButton.Enabled = false;
+                    pauseButton.Enabled = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid number in textbox.");
+
+                startNumTextBox.Enabled = true;
+                endNumTextBox.Enabled = true;
+                startButton.Enabled = true;
+                progressBar1.Visible = false;
+                cancelButton.Enabled = false;
+                pauseButton.Enabled = false;
+            }
+
+
+        }
+
+        private delegate void UpdateLabelCallback(int i);
+
+        private void AddNumberToLabel(int num)
+        {
+            // See if we are currently on the same thread as the control. 
+            // We won't be if the DoWork() method calls this sub, but we will be 
+            // when invoked via the delegate 
+            if (numbersTextBox.InvokeRequired)
+            {
+                // Invoke the UpdateLabelCallback delegate on the UI thread 
+                UpdateLabelCallback callback = new UpdateLabelCallback(AddNumberToLabel);
+                try
+                {
+                    this.Invoke(callback, new object[] { num });
+                }
+                catch (ObjectDisposedException e)
+                {
+                    if (backgroundWorker1.IsBusy)
+                    {
+                        backgroundWorker1.CancelAsync();
+                    }
+                    
+                }
+            }
+            else
+                numbersTextBox.AppendText(num + "\n");
+        } 
+
+        private bool IsPrime(int num)
+        {
+            // Look for a number that evenly divides the num
+            for (int i = 2; i < num / 2; i++)
+                if (num % i == 0)
+                    return false;
+
+            // No divisors means the number is prime
+            return true;
+        }
+
+        //PAUSE
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            // Pause or resume the current job
+            if (pauseButton.Text == "Pause")
+            {
+                pauseButton.Text = "Resume";
+            }
+            else
+            {
+                pauseButton.Text = "Pause";
+            }
+            
+        }
+
+        //CANCEL
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation)
+                backgroundWorker1.CancelAsync();
+        }
+
+        //DOWORK
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            // Find all prime numbers starting between the first and last numbers
+            int first = Convert.ToInt32(startNumTextBox.Text);
+            int last = Convert.ToInt32(endNumTextBox.Text);
+
+            if (first > last)
+            {
+                last = Convert.ToInt32(startNumTextBox.Text);
+                first = Convert.ToInt32(endNumTextBox.Text);
+            }
+
+            // See which numbers are factors and append them to the numbers text box
+            for (int i = first; i < last; i++)
+            {
+                // Leave the loop when the Cancel button has been pressed
+                // (This won't actually work because the UI thread is tied-up.)
+                if(worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    if (pauseButton.Text == "Pause")
+                    {
+                        // Make the progress bar show the percent we've completed
+                        worker.ReportProgress(i);
+
+                        if (IsPrime(i))
+                            AddNumberToLabel(i);
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(25);
+                    }
+
+                    if (cancelJob)
+                        break;
+
+                }
+            }
+        }
+
+        //PROGRESS CHANGED
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        //RUN WORKER COMPLETED
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+                startNumTextBox.Enabled = true;
+                endNumTextBox.Enabled = true;
+                startButton.Enabled = true;
+                progressBar1.Visible = false;
+                cancelButton.Enabled = false;
+                pauseButton.Enabled = false;
+
+           if (pauseButton.Text == "Resume")
+            {
+                pauseButton.Text = "Pause";
+            }
+        }
+    }
+}
